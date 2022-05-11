c38e1e4198ee03e3c8478b0f849c697039eb1dc8
João Guilherme <joaoguilhermefagundes@gmail.com>
2022-05-10 21:25:41 -0300

    subindo alterações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,8 +6,7 @@ import { AppComponent } from './app.component';
 import { NavigationModule } from './navigation/navigation.module';
 import { ClientesModule } from './pages/clientes/clientes.module';
 import { ProdutosListComponent } from './pages/produtos/produtos-list/produtos-list.component';
-import { SobreComponent } from './pages/sobre/sobre.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SobreComponent } from './pages/sobre/sobre.component'
 
 @NgModule({
   declarations: [
@@ -20,8 +18,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     NavigationModule,
-    ClientesModule,
-    NgbModule
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
