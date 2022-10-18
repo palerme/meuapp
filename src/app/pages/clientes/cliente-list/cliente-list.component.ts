d71b246ec1725205aef01212629a20a556dcce81
João Guilherme <joaoguilhermefagundes@gmail.com>
2022-10-18 14:19:10 -0300

    datadownload test

--- src/app/pages/clientes/cliente-list/cliente-list.component.ts
+++ src/app/pages/clientes/cliente-list/cliente-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Clientes } from '../shared/clientes.model';
 import { ClientesService } from '../shared/clientes.servicos';
+import * as XLSX from 'xlsx'
 
 @Component({
   selector: 'jg-cliente-list',
@@ -10,6 +11,7 @@ import { ClientesService } from '../shared/clientes.servicos';
 export class ClientesListComponent implements OnInit {
 
   clientes: Clientes[] = [];
+  Company: any;
 
   constructor(private service: ClientesService) { }
 
@@ -17,11 +19,24 @@ export class ClientesListComponent implements OnInit {
     this.findAll();
   }
 
-  findAll(): void{
+  findAll(): void {
     this.service.findAll().subscribe(
-      clientes => this.clientes = clientes, 
-      error => alert('Erro ao carregar a lista') 
+      clientes => this.clientes = clientes,
+      error => alert('Erro ao carregar a lista')
     )
+  }
+
+  fileName = 'cadastroDeCliente.xls'
+
+  exportexcel(): void {
+    
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+    
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    XLSX.writeFile(wb, this.fileName);
   }
 
 }
